1d98283247b91d23602bba073ef149b7c6545e22
k <karivartech@gmail.com>
2012-06-06 22:32:16 +0200

    Template app.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Karivartech::Application.config.secret_token = '0e74e4f526b6f96e5762e5264d75cd8ba19b2594df104ebbe94f3a9caca86955a24ab25b7539cfb8543af74d8b7eb1fa16e59965a667e9d04773360bc6d3019b'
